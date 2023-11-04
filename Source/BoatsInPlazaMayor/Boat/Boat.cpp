8b6fa6340187ba63697a749b1d5db9365378ff36
alvcabre <alvarocabrerobarros@gmail.com>
2023-11-04 22:54:25 +0100

    Shoot with boat

--- Source/BoatsInPlazaMayor/Boat/Boat.cpp
+++ Source/BoatsInPlazaMayor/Boat/Boat.cpp
@@ -10,143 +10,145 @@
 #include "Kismet/KismetMathLibrary.h"
 #include "Camera/CameraComponent.h"
 #include "GameFramework/SpringArmComponent.h"
+#include "BoatsInPlazaMayor/Components/BoatShootComponent.h"
 
 // Sets default values
 ABoat::ABoat()
 {
- 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+  // Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+  PrimaryActorTick.bCanEverTick = true;
 
-    Root = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Root"));
-    BoatMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BoatMesh"));
-    BoatMesh->SetupAttachment(Root);
+  Root = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Root"));
+  BoatMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BoatMesh"));
+  BoatMesh->SetupAttachment(Root);
 
-    // Don't rotate when the controller rotates. Let that just affect the camera.
-    bUseControllerRotationPitch = false;
-    bUseControllerRotationYaw = false;
-    bUseControllerRotationRoll = false;
+  // Don't rotate when the controller rotates. Let that just affect the camera.
+  bUseControllerRotationPitch = false;
+  bUseControllerRotationYaw = false;
+  bUseControllerRotationRoll = false;
 
-    // Create a camera boom (pulls in towards the player if there is a collision)
-    CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
-    CameraBoom->SetupAttachment(Root);
-    CameraBoom->TargetArmLength = 400.0f; // The camera follows at this distance behind the character	
-    CameraBoom->bUsePawnControlRotation = true; // Rotate the arm based on the controller
+  // Create a camera boom (pulls in towards the player if there is a collision)
+  CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+  CameraBoom->SetupAttachment(Root);
+  CameraBoom->TargetArmLength = 400.0f; // The camera follows at this distance behind the character	
+  CameraBoom->bUsePawnControlRotation = true; // Rotate the arm based on the controller
 
-    // Create a follow camera
-    FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
-    FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName); // Attach the camera to the end of the boom and let the boom adjust to match the controller orientation
-    FollowCamera->bUsePawnControlRotation = false; // Camera does not rotate relative to arm
+  // Create a follow camera
+  FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+  FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName); // Attach the camera to the end of the boom and let the boom adjust to match the controller orientation
+  FollowCamera->bUsePawnControlRotation = false; // Camera does not rotate relative to arm
 
+  BoatShootComponent = CreateDefaultSubobject<UBoatShootComponent>(TEXT("BoatShootComponent"));
 }
 
 // Called when the game starts or when spawned
 void ABoat::BeginPlay()
 {
-	Super::BeginPlay();
+  Super::BeginPlay();
 
-    //Add Input Mapping Context
-    if (APlayerController* PlayerController = Cast<APlayerController>(Controller))
+  //Add Input Mapping Context
+  if (APlayerController* PlayerController = Cast<APlayerController>(Controller))
+  {
+    if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
     {
-        if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
-        {
-            Subsystem->AddMappingContext(DefaultMappingContext, 0);
-        }
+      Subsystem->AddMappingContext(DefaultMappingContext, 0);
     }
-    	
+  }
+
 }
 
 // Called every frame
 void ABoat::Tick(float DeltaTime)
 {
-	Super::Tick(DeltaTime);
+  Super::Tick(DeltaTime);
 }
 
 // Called to bind functionality to input
 void ABoat::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
-	//Super::SetupPlayerInputComponent(PlayerInputComponent);
+  //Super::SetupPlayerInputComponent(PlayerInputComponent);
 
     // Set up action bindings
-    if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent)) {
+  if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent)) {
 
-        // Jumping
-        //EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Started, this, &ACharacter::Jump);
-        //EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Completed, this, &ACharacter::StopJumping);
+    // Jumping
+    //EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Started, this, &ACharacter::Jump);
+    //EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Completed, this, &ACharacter::StopJumping);
 
-        // Moving
-        EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ABoat::Move);
+    // Moving
+    EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ABoat::Move);
 
-        // Looking
-        EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &ABoat::Look);
-    }
-    else
-    {
-        UE_LOG(LogTemplateCharacter, Error, TEXT("'%s' Failed to find an Enhanced Input component! This template is built to use the Enhanced Input system. If you intend to use the legacy system, then you will need to update this C++ file."), *GetNameSafe(this));
-    }
+    // Looking
+    EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &ABoat::Look);
+  }
+  else
+  {
+    UE_LOG(LogTemplateCharacter, Error, TEXT("'%s' Failed to find an Enhanced Input component! This template is built to use the Enhanced Input system. If you intend to use the legacy system, then you will need to update this C++ file."), *GetNameSafe(this));
+  }
 }
 
 void ABoat::Move(const FInputActionValue& Value)
 {
-    // input is a Vector2D
-    FVector2D MovementVector = Value.Get<FVector2D>();
-    FVector Velocity = GetVelocity();
-    int Speed = (int)Velocity.Length();
-   
-
-    //Calculo de fuerza
-    float ForceX = MovementVector.X  ;
-    float ForceY = MovementVector.Y;
+  // input is a Vector2D
+  FVector2D MovementVector = Value.Get<FVector2D>();
+  FVector Velocity = GetVelocity();
+  int Speed = (int)Velocity.Length();
 
 
-    FVector Forward = Root->GetForwardVector();
-    FVector Right = Root->GetRightVector();
+  //Calculo de fuerza
+  float ForceX = MovementVector.X;
+  float ForceY = MovementVector.Y;
 
-    FVector ForceXToAdd = Right * ForceX*100*0.05;
-    FVector ForceYToAdd = Forward * ForceY*0.05;
 
-    // Aplicamos la fuerza
-    if (Controller != nullptr)
-    {
-        Root->AddForce(ForceXToAdd * Root->GetMass(), NAME_None, true);
-        Root->AddForce(ForceYToAdd * Root->GetMass() , NAME_None , true );
-    }
+  FVector Forward = Root->GetForwardVector();
+  FVector Right = Root->GetRightVector();
 
-    // Calculo de rotacion 
+  FVector ForceXToAdd = Right * ForceX * 100 * 0.05;
+  FVector ForceYToAdd = Forward * ForceY * 0.05;
 
-    FVector xVector{ GetActorRotation().Roll, 0.0f, 0.0f };
+  // Aplicamos la fuerza
+  if (Controller != nullptr)
+  {
+    Root->AddForce(ForceXToAdd * Root->GetMass(), NAME_None, true);
+    Root->AddForce(ForceYToAdd * Root->GetMass(), NAME_None, true);
+  }
 
-    FRotator DesiredRotation = UKismetMathLibrary::MakeRotationFromAxes(Velocity, FVector::CrossProduct(FVector::UpVector, Velocity), FVector::UpVector);
+  // Calculo de rotacion 
 
-    FRotator ActorRotation = GetActorRotation();
+  FVector xVector{ GetActorRotation().Roll, 0.0f, 0.0f };
 
-    //UE_LOG(LogTemplateCharacter, Error, TEXT("FuerzaX: '%d'"), ForceXToAdd.Length());
-    UE_LOG(LogTemplateCharacter, Error, TEXT("Velocidad: '%i'"), (int)Velocity.Length());
+  FRotator DesiredRotation = UKismetMathLibrary::MakeRotationFromAxes(Velocity, FVector::CrossProduct(FVector::UpVector, Velocity), FVector::UpVector);
 
-    FRotator lerpedvalue = ActorRotation;
-    if ((int)Velocity.Length() < 180) {
-        lerpedvalue = FMath::RInterpTo(ActorRotation, DesiredRotation, GetWorld()->GetDeltaSeconds(),  0.0010f);
-    }
-    else {
-       lerpedvalue = FMath::RInterpTo(ActorRotation, DesiredRotation, GetWorld()->GetDeltaSeconds(), 100.0f);
-    }
-    // Aplicamos la rotacion
-    if (MovementVector.Y != -1) {
-        SetActorRotation(FRotator(ActorRotation.Pitch, lerpedvalue.Yaw, ActorRotation.Roll));
-    }
+  FRotator ActorRotation = GetActorRotation();
+
+  //UE_LOG(LogTemplateCharacter, Error, TEXT("FuerzaX: '%d'"), ForceXToAdd.Length());
+  UE_LOG(LogTemplateCharacter, Error, TEXT("Velocidad: '%i'"), (int)Velocity.Length());
+
+  FRotator lerpedvalue = ActorRotation;
+  if ((int)Velocity.Length() < 180) {
+    lerpedvalue = FMath::RInterpTo(ActorRotation, DesiredRotation, GetWorld()->GetDeltaSeconds(), 0.0010f);
+  }
+  else {
+    lerpedvalue = FMath::RInterpTo(ActorRotation, DesiredRotation, GetWorld()->GetDeltaSeconds(), 100.0f);
+  }
+  // Aplicamos la rotacion
+  if (MovementVector.Y != -1) {
+    SetActorRotation(FRotator(ActorRotation.Pitch, lerpedvalue.Yaw, ActorRotation.Roll));
+  }
 
 
 }
 
 void ABoat::Look(const FInputActionValue& Value)
 {
-    // input is a Vector2D
-    FVector2D LookAxisVector = Value.Get<FVector2D>();
+  // input is a Vector2D
+  FVector2D LookAxisVector = Value.Get<FVector2D>();
 
-    if (Controller != nullptr)
-    {
-        // add yaw and pitch input to controller
-        AddControllerYawInput(LookAxisVector.X);
-        AddControllerPitchInput(LookAxisVector.Y);
-    }
+  if (Controller != nullptr)
+  {
+    // add yaw and pitch input to controller
+    AddControllerYawInput(LookAxisVector.X);
+    AddControllerPitchInput(LookAxisVector.Y);
+  }
 }
 
