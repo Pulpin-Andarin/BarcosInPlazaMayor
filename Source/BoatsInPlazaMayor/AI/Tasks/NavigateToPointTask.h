d88db1f77250d11b6e76b75e363fc97bdb77e250
Hoku <horoyoku@gmail.com>
2023-11-04 15:23:55 +0100

    Starting ships AI

---
+++ Source/BoatsInPlazaMayor/AI/Tasks/NavigateToPointTask.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/Tasks/BTTask_MoveTo.h"
+#include "NavigateToPointTask.generated.h"
+
+
+UCLASS()
+class BOATSINPLAZAMAYOR_API UNavigateToPointTask : public UBTTask_MoveTo
+{
+	GENERATED_BODY()
+
+	
+};
