1be10861b0d7c67d073c45f223d91b5320ebe3d2
alvcabre <alvarocabrerobarros@gmail.com>
2023-11-05 16:08:13 +0100

    reorganizing files

--- Source/BoatsInPlazaMayor/Components/HealthComponent.cpp
+++ Source/BoatsInPlazaMayor/Components/HealthComponent.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "Components/HealthComponent.h"
+#include "HealthComponent.h"
 
 // Sets default values for this component's properties
 UHealthComponent::UHealthComponent()
